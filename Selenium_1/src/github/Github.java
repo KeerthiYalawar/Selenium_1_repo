9ecaa1abae8eadcafc6b076e8fe6d9161bc4e12b
decent-rajat <rajatprasad2912@gmail.com>
2024-11-15 13:48:40 +0530

    push code

---
+++ Selenium_1/src/github/Github.java
@@ -0,0 +1,13 @@
+package github;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Github{
+	@Test
+	
+	
+	public void addMessage() {
+		Reporter.log("welcome to github",true);
+	}
+}
